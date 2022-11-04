978ce6c2cfb4cd37896eb9026d2d4c29eebf9126
Xinyuan Miao <mxinyuan@foxmail.com>
2022-11-04 13:27:41 +0800

    Add gcov4tvm.patch and script for building tvm-gcov. Hide the building for tvm-no-cov and tvm-libfuzz.

--- tvm_cov_patch/build_tvm.sh
+++ tvm_cov_patch/build_tvm.sh
@@ -2,7 +2,23 @@ set -e
 
 echo "Installing latest TVM... If it fails you may want to use earlier versions."
 git clone https://github.com/apache/tvm.git --recursive
+# tvm-v0.8-release
 cd tvm && git reset --hard 7b3a22e465dd6aca4729504a19beb4bc23312755 && cd ..
+cp -r tvm tvm-gcov
+echo "Start building TVM with Gcov"
+cd tvm-gcov
+mkdir -p build
+cp cmake/config.cmake build/
+patch -p1 < ../gcov4tvm.patch
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DENABLE_GCOV=1
+make -j$(nproc)
+cd ..
+echo "Successfully installed TVM-gcov!"
+echo "To use TVM-gcov, copy and paste the following shell statements:"
+echo "export TVM_HOME=$(pwd)"
+echo "export PYTHONPATH=$TVM_HOME/python"
+
 cp -r tvm tvm-no-cov
 
 echo "Start building TVM with coverage instrumentation"
@@ -22,35 +38,35 @@ echo "To use TVM-cov, copy and paste the following shell statements:"
 echo "export TVM_HOME=$(pwd)"
 echo "export PYTHONPATH=$TVM_HOME/python"
 
-echo "Start building TVM without coverage instrumentation"
-cd ../tvm-no-cov
-mkdir -p build && cd build
-cp ../../tvm/cmake/config.cmake . # Sync cmake file with coverage version.
-cmake .. -DCMAKE_BUILD_TYPE=Release \
-         -DCMAKE_CXX_COMPILER=$(which clang++) \
-         -DCMAKE_C_COMPILER=$(which clang) \
-         -DFETCHCONTENT_QUIET=0
-make -j$(nproc)
-cd ..
-echo "Successfully installed TVM w/o coverage!"
-echo "To use TVM-cov, copy and paste the following shell statements:"
-echo "export TVM_HOME=$(pwd)"
-echo "export PYTHONPATH=$TVM_HOME/python"
+#echo "Start building TVM without coverage instrumentation"
+#cd ../tvm-no-cov
+#mkdir -p build && cd build
+#cp ../../tvm/cmake/config.cmake . # Sync cmake file with coverage version.
+#cmake .. -DCMAKE_BUILD_TYPE=Release \
+#         -DCMAKE_CXX_COMPILER=$(which clang++) \
+#         -DCMAKE_C_COMPILER=$(which clang) \
+#         -DFETCHCONTENT_QUIET=0
+#make -j$(nproc)
+#cd ..
+#echo "Successfully installed TVM w/o coverage!"
+#echo "To use TVM-cov, copy and paste the following shell statements:"
+#echo "export TVM_HOME=$(pwd)"
+#echo "export PYTHONPATH=$TVM_HOME/python"
 
-echo "Start building LibFuzzer for TVM"
-cd ..
-cp -r tvm tvm-libfuzz
-cd tvm-libfuzz
-rm -rf build
-patch -p1 < ../libfuzz.patch
-mkdir -p build && cd build
-cp ../../tvm/cmake/config.cmake . # Sync cmake file with coverage version.
-cmake .. -DCMAKE_BUILD_TYPE=Release \
-         -DCMAKE_CXX_COMPILER=$(which clang++) \
-         -DCMAKE_C_COMPILER=$(which clang) \
-         -DFETCHCONTENT_QUIET=0
-make -j$(nproc)
-cd ..
-echo "Successfully installed LibFuzzer for TVM"
-echo "To run the libfuzzer driver, simply run ./build/fuzz_me"
-echo "You need to manually stop fuzz_me otherwise it will run forever."
+#echo "Start building LibFuzzer for TVM"
+#cd ..
+#cp -r tvm tvm-libfuzz
+#cd tvm-libfuzz
+#rm -rf build
+#patch -p1 < ../libfuzz.patch
+#mkdir -p build && cd build
+#cp ../../tvm/cmake/config.cmake . # Sync cmake file with coverage version.
+#cmake .. -DCMAKE_BUILD_TYPE=Release \
+#         -DCMAKE_CXX_COMPILER=$(which clang++) \
+#         -DCMAKE_C_COMPILER=$(which clang) \
+#         -DFETCHCONTENT_QUIET=0
+#make -j$(nproc)
+#cd ..
+#echo "Successfully installed LibFuzzer for TVM"
+#echo "To run the libfuzzer driver, simply run ./build/fuzz_me"
+#echo "You need to manually stop fuzz_me otherwise it will run forever."
